14322a0f401c66961f81749155d8c541784be56e
Maximilian Mayrhauser <maximilian.mayrhauser@gmail.com>
2024-04-13 17:35:32 +0200

    fix so that output file remains unchanged

--- A0/Llewi-HelloWorld.py
+++ A0/Llewi-HelloWorld.py
@@ -1,12 +1,10 @@
 input = open("HelloWorld-test1.in", "r")
-lines = input.read().splitlines()
+lines = input.readlines()
 input.close()
 
 output = open("HelloWorld-test1.out", "w")
 output.write("Hello World!\n")
-for i, line in enumerate(lines):
+for line in lines:
     output.write(line)
-    if i < len(output) - 1:
-        output.write("\n")
 
 output.close()
