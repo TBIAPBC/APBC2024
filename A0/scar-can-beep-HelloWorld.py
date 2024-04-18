bad02e5163746273f8fb702cda738a214c085385
scar-can-beep <rian.scar@gmx.at>
2024-04-18 17:22:28 +0200

    changed output and terminal input

--- A0/scar-can-beep-HelloWorld.py
+++ A0/scar-can-beep-HelloWorld.py
@@ -1,4 +1,12 @@
-output = open('HelloWorld-test1.out', 'w')
-output.write('Hello World!\n')
-with open('HelloWorld-test1.in', 'r') as hello_input:
-    output.write(hello_input.readline())
+import argparse
+
+#PARSER
+parser = argparse.ArgumentParser()
+parser.add_argument('file', help='Insert input file path here')
+args = parser.parse_args()
+file_location = args.file
+
+#MAIN
+print('Hello World!')
+with open(file_location, 'r') as hello_input:
+    print(hello_input.readline(),end='')
