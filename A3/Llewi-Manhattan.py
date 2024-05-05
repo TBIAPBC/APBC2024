28ef8840cf80081a104ec15cedd72b2fef32162e
Maximilian Mayrhauser <maximilian.mayrhauser@gmail.com>
2024-05-05 21:57:42 +0200

    rename file and clean up the code

--- A3/Llewi-Manhattan.py
+++ A3/Llewi-Manhattan.py
@@ -10,18 +10,15 @@ def read_input(input_text, diagonal):
     # Remove comments and strip whitespace characters, keep only non-empty lines
     input_text = [line.split("#")[0].strip() for line in input_text if line.split("#")[0].strip()]
 
-    # Column count can be read from first input line
-    n_col = len(input_text[0].split())
-
+    # Determine dimensions
     # For row count, iterate through lines until the number of characters doesn't match anymore
+    n_col = len(input_text[0].split())
     for n_row, line in enumerate(input_text):
         if n_col != len(line.split()):
             n_row += 1
             break
 
-    print("Dimensions")
-    print(f" {n_row} {n_col}")
-    # Initialise weight matrices with -math.inf, as symmetric matrices for easier handling
+    # Initialise weight matrices with -math.inf
     # Invalid paths (e.g. going east in the eastmost cities) will be prohibited by a value of negative infinity, so they will never be chosen
     from_north = np.full((n_row, n_col), -math.inf)
     from_west = from_north.copy()
@@ -35,7 +32,7 @@ def read_input(input_text, diagonal):
     # Fill east matrix
     line_offset = n_row - 1
     for i in range(n_row):
-        # Leave first row at -inf
+        # Leave first column at -inf
         from_west[i, 1:] = input_text[i+line_offset].split()
 
     # Fill diagonal matrix
@@ -47,13 +44,13 @@ def read_input(input_text, diagonal):
     return from_north, from_west, from_diag
 
 
-def logic_now(from_north, from_west, from_diag, diagonal, trace):
+def compute_dynamic_programming_mat(from_north, from_west, from_diag):
     n_row, n_col = from_north.shape
 
     # initialise dynamic programming matrix (requires additional row and column for initialisation)
     dp_mat = np.full((n_row + 1, n_col + 1), -math.inf)
 
-    # Store directions during algorithm - leads to higher memory requirements but requires no computation in backtracing
+    # Alternative implementation - store directions during algorithm. Leads to higher memory requirements but requires no computation in backtracing
     backtrace_mat = [["-"] * n_col for _ in range(n_row)]
 
     # Walk through matrix column by column, row by row and build optimal partial solution.
@@ -79,30 +76,20 @@ def logic_now(from_north, from_west, from_diag, diagonal, trace):
             # Assign highest scoring value
             dp_mat[dp_row, dp_col] = np.max([from_north_val, from_west_val, from_diag_val])
 
-            # Store direction corresponding to highest scoring value
+            # Alternative implementation - store direction corresponding to highest scoring value
             direction_index = np.argmax([from_north_val, from_west_val, from_diag_val])
             backtrace_mat[row_id][col_id] = ["S", "E", "D"][direction_index]
-            # debug print
-            #if row_id==0 and col_id < 3:
-            #    print(f"Step {col_id}")
-            #    print(f"update value at {row_id}/{col_id}")
-            #    print(f"max of {from_north_val} {from_west_val} {from_diag_val}")
-            #    print(dp_mat)
 
-    dp_row = n_row
-    dp_col = n_col
+    return dp_mat, backtrace_mat
+
+
+def backtrace(dp_mat, from_north, from_west, from_diag):
+    dp_row, dp_col = from_north.shape
     path = ""
-    i = 0
     while dp_col > 1 or dp_row > 1:
         row_id = dp_row - 1
         col_id = dp_col - 1
-        i += 1
-
         current_field = dp_mat[dp_row, dp_col]
-
-        print(path)
-        print(f"{row_id} {col_id}")
-
 
         # Is route from north possible?
         if current_field == dp_mat[dp_row - 1, dp_col] + from_north[row_id, col_id]:
@@ -122,14 +109,31 @@ def logic_now(from_north, from_west, from_diag, diagonal, trace):
             dp_col -= 1
             path = "D" + path
             continue
+        # If not, that's an error
         else:
-            stop("This isn't working")
-        
-    print("don2)")
-    print(path)        
+            exit("This isn't working")
+    
+    return path
 
 
-
+def backtrace_from_directions_mat(backtrace_mat):
+    row_id = len(backtrace_mat) - 1
+    col_id = len(backtrace_mat[0]) - 1
+    path = ""
+    while row_id > 0 or col_id > 0:
+        direction = backtrace_mat[row_id][col_id]
+        path = direction + path
+        if direction == "S":
+            row_id -= 1
+        elif direction == "E":
+            col_id -= 1
+        elif direction == "D":
+            row_id -= 1
+            col_id -= 1
+        else:
+            exit("This isn't working")
+    
+    return path
 
 
 if __name__ == "__main__":
@@ -139,6 +143,7 @@ if __name__ == "__main__":
     parser.add_argument("filename", nargs="*", default=argparse.SUPPRESS)
     parser.add_argument("-d", "--diagonal", action="store_true")
     parser.add_argument("-t", "--trace", action="store_true")
+    parser.add_argument("-a", "--alternative_backtrace_implementation", action="store_true")
     parser.add_argument("-v", "--verbose", action="store_true")
     #parser.add_argument("-o", "--optimize", action="store_true")
     args = parser.parse_args()
@@ -158,12 +163,20 @@ if __name__ == "__main__":
     # Parse file
     from_north, from_west, from_diag = read_input(input_text, args.diagonal)
 
-    if args.verbose:
-        print("south weights")
-        print(from_north)
-        print("east weights")
-        print(from_west)
-        print("diag weights")
-        print(from_diag)
+    # Run logic
+    dp_mat, backtrace_mat = compute_dynamic_programming_mat(from_north, from_west, from_diag)
 
-    logic_now(from_north, from_west, from_diag, args.diagonal, args.trace)
+    # Print weight
+    max_weight = dp_mat[-1, -1]
+    if max_weight.is_integer():
+        print(int(max_weight))
+    else:
+        print(np.format_float_positional(max_weight, 2))
+
+    # Backtrace if you want
+    if args.trace:
+        if args.alternative_backtrace_implementation:
+            path = backtrace_from_directions_mat(backtrace_mat)
+        else:
+            path = backtrace(dp_mat, from_north, from_west, from_diag)
+        print(path)
