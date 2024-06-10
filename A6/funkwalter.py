54475b824ecf97462b4ae13edab88c0b8e44b9c8
Stefan Kalteis <s.kalteis@gmx.net>
2024-06-10 13:10:19 +0200

    trying djikstra and new logic

--- A6/funkwalter.py
+++ A6/funkwalter.py
@@ -1,27 +1,25 @@
+import heapq
+import numpy as np
+from game_utils import Direction as D, TileStatus, Map
 from player_base import Player
-from game_utils import Direction as D, TileStatus, TileObject, Map
-from shortestpaths import AllShortestPaths
-import random
-
 
 class funkwalter(Player):
+
     def __init__(self, *, random=True):
         self.random = random
-        self.goldPotRemainingRounds = None
-        self.known_positions = {}
 
     def reset(self, player_id, max_players, width, height):
-        self.player_name = "funkwalter"
-        self.player_id = player_id  # Store player_id here
-        self.width = width
-        self.height = height
+        self.player_name = "funkWalter"
         self.ourMap = Map(width, height)
-        self.goldPotRemainingRounds = None
-        self.known_positions.clear()
+        self.visited = set()
+        self.exploring = True
+        self.round_counter = 0
+        self.gold_pot_reset_round = 0
+        self.last_gold_pot_location = None
+        self.player_id = player_id
 
     def round_begin(self, r):
-        # Reset or update state variables at the beginning of each round
-        pass
+        self.round_counter = r
 
     def _as_direction(self, curpos, nextpos):
         for d in D:
@@ -35,25 +33,64 @@ class funkwalter(Player):
 
     def _is_within_bounds(self, pos):
         x, y = pos
-        return 0 <= x < self.width and 0 <= y < self.height
+        return 0 <= x < self.ourMap.width and 0 <= y < self.ourMap.height
 
-    def _avoid_mines_and_robots(self, path):
-        # Implement logic to avoid mines and other robots
-        safe_path = []
-        for pos in path:
-            if self.ourMap[pos].status != TileStatus.Mine and pos not in self.known_positions.values():
-                safe_path.append(pos)
-        return safe_path
+    def _explore(self, curpos, numMoves):
+        moves = []
+        for _ in range(numMoves):
+            neighbors = []
+            for d in D:
+                diff = d.as_xy()
+                new_pos = curpos[0] + diff[0], curpos[1] + diff[1]
 
-    def _explore_unknown(self, curpos):
-        # Implement logic to explore unknown areas
-        directions = list(D)
-        random.shuffle(directions)
-        for d in directions:
-            new_pos = (curpos[0] + d.as_xy()[0], curpos[1] + d.as_xy()[1])
-            if self._is_within_bounds(new_pos) and self.ourMap[new_pos].status == TileStatus.Unknown:
-                return [new_pos]
-        return []
+                if self._is_within_bounds(new_pos) and new_pos not in self.visited:
+                    tile = self.ourMap[new_pos[0], new_pos[1]]
+                    if tile.status == TileStatus.Unknown or tile.status == TileStatus.Empty:
+                        neighbors.append((d, new_pos))
+            if not neighbors:
+                break
+
+            move = neighbors[0][0]
+            if move:
+                moves.append(move)
+                diff = move.as_xy()
+                curpos = curpos[0] + diff[0], curpos[1] + diff[1]
+                self.visited.add(curpos)
+        return moves
+
+    def dijkstra(self, start, goal):
+        width, height = self.ourMap.width, self.ourMap.height
+        dist = np.full((width, height), np.inf)
+        dist[start] = 0
+        priority_queue = [(0, start)]
+        heapq.heapify(priority_queue)
+        came_from = {}
+
+        while priority_queue:
+            current_dist, current = heapq.heappop(priority_queue)
+
+            if current == goal:
+                break
+
+            for d in D:
+                diff = d.as_xy()
+                neighbor = (current[0] + diff[0], current[1] + diff[1])
+
+                if self._is_within_bounds(neighbor) and self.ourMap[neighbor].status != TileStatus.Wall:
+                    new_dist = current_dist + 1  # All moves cost the same (1)
+                    if new_dist < dist[neighbor]:
+                        dist[neighbor] = new_dist
+                        heapq.heappush(priority_queue, (new_dist, neighbor))
+                        came_from[neighbor] = current
+
+        # Reconstruct path
+        path = []
+        current = goal
+        while current != start:
+            path.append(current)
+            current = came_from.get(current, start)
+        path.reverse()
+        return path
 
     def move(self, status):
         ourMap = self.ourMap
@@ -61,41 +98,55 @@ class funkwalter(Player):
             for y in range(ourMap.height):
                 if status.map[x, y].status != TileStatus.Unknown:
                     ourMap[x, y].status = status.map[x, y].status
+                    self.visited.add((x, y))
 
         curpos = (status.x, status.y)
-        self.known_positions[self.player_id] = curpos  # Store position of this robot
 
         assert len(status.goldPots) > 0
         gLoc = next(iter(status.goldPots))
-        self.goldPotRemainingRounds = status.goldPotRemainingRounds
 
-        paths = AllShortestPaths(gLoc, ourMap)
-        if self.random:
-            bestpath = paths.randomShortestPathFrom(curpos)
-        else:
-            bestpath = paths.shortestPathFrom(curpos)
+        if self.last_gold_pot_location and gLoc != self.last_gold_pot_location:
+            self.gold_pot_reset_round = self.round_counter
 
-        bestpath = bestpath[1:]  # remove the current position
-        bestpath.append(gLoc)
+        self.last_gold_pot_location = gLoc
 
-        bestpath = self._avoid_mines_and_robots(bestpath)
-        if not bestpath:
-            bestpath = self._explore_unknown(curpos)
-
+        bestpath = self.dijkstra(curpos, gLoc)
         distance = len(bestpath)
 
-        # Dynamic halfway distance check based on remaining rounds and map dimensions
-        half_map_distance = min(self.width, self.height) / 2
-        if distance > self.goldPotRemainingRounds or distance > half_map_distance:
-            # Explore if not moving towards the gold pot
-            exploration_path = self._explore_unknown(curpos)
-            if exploration_path:
-                return self._as_directions(curpos, exploration_path)
-            return []  # Stay put if there's no unknown area to explore
+        numMoves = min(4, distance)  # Dynamic adjustment based on distance
 
-        # Move towards the gold pot
-        return self._as_directions(curpos, bestpath)
+        goldPotRemainingRounds = status.goldPotRemainingRounds
+        if distance > goldPotRemainingRounds:
+            numMoves = 0  # Avoid moving if gold pot is out of reach
 
+        if status.gold < numMoves * (numMoves + 1) / 2:
+            numMoves = 0  # Avoid moving if not enough gold to cover the cost of moves
+
+        # Check proximity of other players and mines based on visible characters
+        avoid_move = False
+        for x in range(max(0, gLoc[0] - 4), min(gLoc[0] + 5, ourMap.width)):
+            for y in range(max(0, gLoc[1] - 4), min(gLoc[1] + 5, ourMap.height)):
+                tile_status = status.map[x, y].status
+                if tile_status == '&':
+                    # Avoid moving into a mine
+                    avoid_move = True
+                    break
+                elif tile_status not in (TileStatus.Unknown, TileStatus.Wall, TileStatus.Empty, '&'):
+                    # Found another player
+                    robot_distance = len(self.dijkstra((x, y), gLoc))
+                    if robot_distance <= 4 and distance > 4:
+                        avoid_move = True
+                        break
+            if avoid_move:
+                break
+
+        if avoid_move:
+            numMoves = 0
+
+        moves = self._explore(curpos, numMoves)
+        if not moves:
+            moves = self._as_directions(curpos, bestpath[:numMoves])
+        return moves
 
 players = [funkwalter()]
 
