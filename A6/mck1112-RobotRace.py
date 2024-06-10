17fb5d2c90468dc61e4407b01ff6572baa1a0f90
Maria Kovacs <166513050+mck1112@users.noreply.github.com>
2024-06-10 16:02:18 +0200

    Update mck1112-RobotRace.py
    tried to improve moves and exploration strategy; first try setting mines

--- A6/mck1112-RobotRace.py
+++ A6/mck1112-RobotRace.py
@@ -21,7 +21,7 @@ class PathfindingNode:
 def heuristic(pos1, pos2):
     return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
 
-def a_star_search(start, goal, our_map):
+def a_star_search(start, goal, our_map, avoid_positions=set()):
     start_node = PathfindingNode(start, None, 0, heuristic(start, goal))
     open_list = [start_node]
     closed_list = set()
@@ -47,7 +47,7 @@ def a_star_search(start, goal, our_map):
                 continue
 
             neighbor_tile = our_map[neighbor_pos[0], neighbor_pos[1]]
-            if neighbor_tile.status == TileStatus.Wall:
+            if neighbor_tile.status == TileStatus.Wall or neighbor_pos in avoid_positions:
                 continue
 
             neighbor_node = PathfindingNode(neighbor_pos, current_node,
@@ -64,12 +64,12 @@ def a_star_search(start, goal, our_map):
 
     return []
 
-class Smarty(Player):
+class ImprovedSmarty(Player):
     def __init__(self):
         super().__init__()
-        self.player_name = "Smarty"
-        self.our_map = Map(10, 10)  # Assuming initial map size is 10x10
-        self.visited = [[0] * 10 for _ in range(10)]  # Assuming initial map size is 10x10
+        self.player_name = "ImprovedSmarty"
+        self.our_map = Map(10, 10)
+        self.visited = [[0] * 10 for _ in range(10)]
         self.goal = None
 
     def reset(self, player_id, max_players, width, height, rules=None):
@@ -94,8 +94,10 @@ class Smarty(Player):
                 return d
         return None
 
-    def _get_path_to_goal(self, start, goal):
-        return a_star_search(start, goal, self.our_map)
+    def _get_path_to_goal(self, start, goal, avoid_positions=set()):
+        if goal is None:
+            return []  # If the goal is not set, return an empty path
+        return a_star_search(start, goal, self.our_map, avoid_positions)
 
     def _find_best_gold_pot(self, status):
         current_pos = (status.x, status.y)
@@ -114,41 +116,67 @@ class Smarty(Player):
 
         return best_gold
 
+    def _explore(self, current_pos):
+        unexplored = []
+        for x in range(self.our_map.width):
+            for y in range(self.our_map.height):
+                if self.our_map[x, y].status == TileStatus.Unknown and self.visited[x][y] == 0:
+                    unexplored.append((x, y))
+        if unexplored:
+            nearest_unexplored = min(unexplored, key=lambda pos: heuristic(current_pos, pos))
+            path = self._get_path_to_goal(current_pos, nearest_unexplored)
+            if path and len(path) > 1:
+                return self._as_direction(current_pos, path[1])
+
+        directions = list(D)
+        random.shuffle(directions)
+        for d in directions:
+            new_pos = (current_pos[0] + d.as_xy()[0], current_pos[1] + d.as_xy()[1])
+            if (0 <= new_pos[0] < self.our_map.width and 0 <= new_pos[1] < self.our_map.height and 
+                self.our_map[new_pos[0], new_pos[1]].status != TileStatus.Wall and 
+                self.visited[new_pos[0]][new_pos[1]] == 0):
+                return d
+        return random.choice(directions)
+
+    def set_mines(self, status):
+        mine_positions = []
+        current_pos = (status.x, status.y)
+        if self.goal is None:
+            return mine_positions  # If the goal is None, do not set any mines
+
+        for x in range(current_pos[0] - 3, current_pos[0] + 3):
+            for y in range(current_pos[1] - 3, current_pos[1] + 3):
+                if 0 <= x < self.our_map.width and 0 <= y < self.our_map.height:
+                    if status.map[x, y].obj and status.map[x, y].obj.is_player():
+                        path = self._get_path_to_goal((x, y), self.goal)
+                        if path:
+                            mine_positions.extend(path[:3])  # Place mines on the first three steps of the path
+        return mine_positions
+
     def move(self, status):
         self._update_map(status)
         current_pos = (status.x, status.y)
+        self.visited[current_pos[0]][current_pos[1]] += 1
 
         if not status.goldPots:
-            return [random.choice(list(D))]  # Move randomly if no gold is present
+            return [self._explore(current_pos)]
 
         if self.goal is None or self.goal not in status.goldPots:
             self.goal = self._find_best_gold_pot(status)
-        
-        if self.goal is None:
-            return [random.choice(list(D))]  # Move randomly if no gold is found
 
-        path = self._get_path_to_goal(current_pos, self.goal)
+        if self.goal is None:
+            return [self._explore(current_pos)]
+
+        avoid_positions = set((p.x, p.y) for p in status.others if p)
+        path = self._get_path_to_goal(current_pos, self.goal, avoid_positions)
+
         if not path:
             self.goal = None
-            return [random.choice(list(D))]  # Move randomly if no path is found
+            return [self._explore(current_pos)]
 
         next_move = path[1] if len(path) > 1 else path[0]
         direction = self._as_direction(current_pos, next_move)
-        
-        return [direction] if direction else [random.choice(list(D))]
 
-players = [Smarty()]
+        return [direction] if direction else [self._explore(current_pos)]
 
-
-
-
-
-# result 
-# Player   Health   Gold      Position
-# smarty   100      1836      10,11
-# test-1   100      1340      10,12
-# test-2   35       2         29,29
-# beatme   100      2542      27,12
-
-# not that efficient as we wanted it to be
-# needs to be improved not that smart yet
+players = [ImprovedSmarty()]
